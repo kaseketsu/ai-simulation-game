1a25eba753c05a9ff318c39e1ed492f6e61b2134
kaseketsu <755740189@qq.com>
2025-12-21 19:44:55 +0800

    feat: 项目初始化

---
+++ src/main/java/com/flower/game/AiSimulationGameApplication.java
@@ -0,0 +1,13 @@
+package com.flower.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSimulationGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSimulationGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flower/game/AiSimulationGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSimulationGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
