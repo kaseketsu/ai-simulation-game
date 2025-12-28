be008e44956643f8cebf20a1997df72d1c9a1c3c
kaseketsu <755740189@qq.com>
2025-12-28 19:43:38 +0800

    feat: 新增 jwt 权限过滤器

--- src/main/java/common/filters/JwtBlackListFilter.java
+++ src/main/java/common/filters/JwtBlackListFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -40,7 +41,7 @@ public class JwtBlackListFilter extends OncePerRequestFilter {
      * @throws IOException      e2
      */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = jwtManager.extractTokenFromHttpRequest(request);
             if (StrUtil.isNotBlank(token)) {
